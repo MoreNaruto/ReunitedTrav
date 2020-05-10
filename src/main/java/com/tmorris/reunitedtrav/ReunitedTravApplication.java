6e731d5055b97cbb564a84e43c0fae3b8934ccf4
T-Mo Morris <morenaruto007@gmail.com>
2020-05-09 19:58:52 -0500

    Setting up projectt

---
+++ src/main/java/com/tmorris/reunitedtrav/ReunitedTravApplication.java
@@ -0,0 +1,13 @@
+package com.tmorris.reunitedtrav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReunitedTravApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReunitedTravApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmorris/reunitedtrav/ReunitedTravApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmorris.reunitedtrav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReunitedTravApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
